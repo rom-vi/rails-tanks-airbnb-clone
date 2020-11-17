0fc1f9a4459914f5433e1274b0c8e941f719f9bb
lorenz westner <mynlexi@gmail.com>
2020-11-17 12:38:01 +0100

    seeds

--- app/models/tank.rb
+++ app/models/tank.rb
@@ -5,7 +5,6 @@ class Tank < ApplicationRecord
 
   validates :name, presence: true
   validates :weight_in_kg, presence: true, numericality: true
-  validates :manufacturer, presence: true
   validates :description, presence: true
   validates :price_per_day, presence: true, numericality: true
   validates :capacity, presence: true, numericality: true
