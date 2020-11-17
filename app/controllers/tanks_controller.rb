7182c8c117786f37de0de490c2481a2a9e7bd384
roman vidal <roman.vidal@stud.uni-regensburg.de>
2020-11-17 13:08:57 +0100

    routen erstellen, tank controller create view

---
+++ app/controllers/tanks_controller.rb
@@ -0,0 +1,25 @@
+class TanksController < ApplicationController
+
+    
+    def new
+        @tank = Tank.new
+    end
+
+    def create
+        @tank = Tank.new(tank_params)
+        if @tank.save
+          flash[:success] = "Tank successfully created"
+          redirect_to @tank
+        else
+          flash[:error] = "Something went wrong"
+          render 'new'
+        end
+    end
+    
+    private
+
+    def tank_params
+        params.require(:tank).permit(:name, :weight_in_kg, :manufacturer, :description, :price_per_day, :capacity, photos: [])
+    end
+
+end
