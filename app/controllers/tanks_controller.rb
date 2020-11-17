fbc6c5f642bfff2ba7eaf54b02eff33daaa206ff
Tim Cecatka <tim.cecatka@gmail.com>
2020-11-17 14:31:25 +0100

    fixed indentation

--- app/controllers/tanks_controller.rb
+++ app/controllers/tanks_controller.rb
@@ -1,29 +1,26 @@
 class TanksController < ApplicationController
-
   def index
     @tanks = current_user.tanks
   end
 
-    
   def new
-      @tank = Tank.new
+    @tank = Tank.new
   end
 
   def create
-      @tank = Tank.new(tank_params)
-      if @tank.save
-        flash[:success] = "Tank successfully created"
-        redirect_to @tank
-      else
-        flash[:error] = "Something went wrong"
-        render 'new'
-      end
+    @tank = Tank.new(tank_params)
+    if @tank.save
+      flash[:success] = "Tank successfully created"
+      redirect_to @tank
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
   end
-  
+
   private
 
   def tank_params
-      params.require(:tank).permit(:name, :weight_in_kg, :manufacturer, :description, :price_per_day, :capacity, photos: [])
+    params.require(:tank).permit(:name, :weight_in_kg, :manufacturer, :description, :price_per_day, :capacity, photos: [])
   end
-
 end
