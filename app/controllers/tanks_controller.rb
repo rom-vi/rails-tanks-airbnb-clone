00248d4ca8b501c6b8e02f5c91456febfb82a847
Tim Cecatka <tim.cecatka@gmail.com>
2020-11-19 12:14:32 +0100

    pundit activated

--- app/controllers/tanks_controller.rb
+++ app/controllers/tanks_controller.rb
@@ -2,12 +2,12 @@ class TanksController < ApplicationController
   before_action :set_tank, only: [:destroy, :show, :edit, :update]
 
   def index
-    @tanks = current_user.tanks 
+    @tanks = policy_scope(current_user.tanks)
   end
 
   def show
+    authorize @tank
   end
-
 
   def new
     @tank = Tank.new
@@ -16,6 +16,7 @@ class TanksController < ApplicationController
   def create
     @tank = Tank.new(tank_params)
     @tank.user = current_user
+    authorize @tank
     if @tank.save!
       flash[:success] = "Tank successfully created"
       redirect_to tanks_path
@@ -26,18 +27,21 @@ class TanksController < ApplicationController
   end
 
   def destroy
+    authorize @tank
     @tank.destroy
     redirect_to tanks_path
   end
 
   def edit
+    authorize @tank
   end
 
   def update
+    authorize @tank
     @tank.update(tank_params)
     redirect_to tank_path(@tank)
   end
-  
+
   private
 
   def set_tank
---
+++ app/policies/tank_policy.rb
@@ -0,0 +1,23 @@
+class TankPolicy < ApplicationPolicy
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+
+  class Scope < Scope
+    def resolve
+      scope
+    end
+  end
+end
